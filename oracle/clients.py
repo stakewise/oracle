8a09f65dd8023082156fe950ed09abfaa4215d88
Dmitri Tsumak <tsumak.dmitri@gmail.com>
2021-11-14 01:46:44 +0200

    Fix fetching ipfs for oracle

--- oracle/clients.py
+++ oracle/clients.py
@@ -53,20 +53,22 @@ async def execute_ethereum_gql_query(query: DocumentNode, variables: Dict) -> Di
 async def ipfs_fetch(ipfs_hash: str) -> Union[Dict[Any, Any], List[Dict[Any, Any]]]:
     """Tries to fetch IPFS hash from different sources."""
     _ipfs_hash = ipfs_hash.replace("ipfs://", "").replace("/ipfs/", "")
-    try:
-        for ipfs_endpoint in IPFS_PIN_ENDPOINTS:
+    for ipfs_endpoint in IPFS_PIN_ENDPOINTS:
+        try:
             with ipfshttpclient.connect(ipfs_endpoint) as client:
                 return client.get_json(_ipfs_hash)
-    except ipfshttpclient.exceptions.TimeoutError:
-        async with ClientSession() as session:
-            for endpoint in IPFS_FETCH_ENDPOINTS:
-                try:
-                    response = await session.get(
-                        f"{endpoint.rstrip('/')}/ipfs/{_ipfs_hash}"
-                    )
-                    response.raise_for_status()
-                    return await response.json()
-                except:  # noqa: E722
-                    pass
+        except ipfshttpclient.exceptions.TimeoutError:
+            pass
+
+    async with ClientSession() as session:
+        for endpoint in IPFS_FETCH_ENDPOINTS:
+            try:
+                response = await session.get(
+                    f"{endpoint.rstrip('/')}/ipfs/{_ipfs_hash}"
+                )
+                response.raise_for_status()
+                return await response.json()
+            except:  # noqa: E722
+                pass
 
     raise RuntimeError(f"Failed to fetch IPFS data at {_ipfs_hash}")
